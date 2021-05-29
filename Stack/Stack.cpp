8d024727431ac4b9a925c8818b4215519f5782f9
Jegyun Kang <tdm1223@naver.com>
2021-05-30 00:44:30 +0900

    [CREATE] STACK

---
+++ Stack/Stack.cpp
@@ -0,0 +1,53 @@
+﻿#include "Stack.h"
+
+int Stack::size()
+{
+    return size_;
+}
+
+Stack::Stack() : size_(0), top_(nullptr)
+{
+    
+}
+
+void Stack::push(int value)
+{
+    std::unique_ptr<Node> newNode(new Node());
+    newNode->data_ = value;
+    size_++;
+    if (empty())
+    {
+        top_ = std::move(newNode);
+    }
+    else
+    {
+        newNode->next_ = std::move(top_);
+        top_ = std::move(newNode);
+    }
+}
+
+void Stack::pop()
+{
+    if (empty())
+    {
+        std::cout << "no data" << std::endl;
+        return;
+    }
+
+    size_--;
+    top_ = std::move(top_->next_);
+}
+
+int Stack::top()
+{
+    return top_->data_;
+}
+
+bool Stack::empty()
+{
+    if (size_ == 0)
+    {
+        return true;
+    }
+    return false;
+}
---
+++ Stack/Stack.h
@@ -0,0 +1,28 @@
+﻿#include<memory>
+#include<iostream>
+
+class Node
+{
+private:
+    int data_;
+    std::unique_ptr<Node> next_;
+public:
+    friend class Stack;
+    Node() : next_(nullptr), data_(NULL) { }
+};
+
+class Stack
+{
+private:
+    int size_;
+    std::unique_ptr<Node> top_;
+
+public:
+    Stack();
+    ~Stack() = default;
+    void push(int value);
+    void pop();
+    int top();
+    bool empty();
+    int size();
+};
